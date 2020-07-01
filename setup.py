bfd521ef387d1855dceec47e918d592c0edc7dfa
Ilyass El Fatimy <46936973+N0x1s@users.noreply.github.com>
2020-07-01 01:43:30 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
     description='A better way to get passwords from users in python',
     author='n0x1s',
     author_email='n0x1s0x01@gmail.com',
-    url='https://github.com/n0x1s/cached_properties',
-    download_url='https://github.com/N0x1s/cached_properties/archive/0.7.tar.gz',
+    url='https://github.com/n0x1s/getpw',
+    download_url='https://github.com/N0x1s/getpw/archive/0.1.tar.gz',
     keywords=['getpass', 'get password', 'mask password'],
     classifiers=[
         'Development Status :: 3 - Alpha',
