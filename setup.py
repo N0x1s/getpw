d98f41ae859a7ef164623666ce8946b2ec0a0056
n0x1s <ilyasselfatimy9@gmail.com>
2020-07-01 02:08:12 +0100

    update some stuff

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
     name='getpw',
     packages=['getpw'],
-    version='0.1',
+    version='0.2',
     license='MIT',
     description='A better way to get passwords from users in python',
     author='n0x1s',
     author_email='n0x1s0x01@gmail.com',
     url='https://github.com/n0x1s/getpw',
-    download_url='https://github.com/N0x1s/getpw/archive/0.1.tar.gz',
+    download_url='https://github.com/N0x1s/getpw/archive/0.2.tar.gz',
     keywords=['getpass', 'get password', 'mask password'],
     classifiers=[
         'Development Status :: 3 - Alpha',
